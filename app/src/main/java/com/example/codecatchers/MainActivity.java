d7b4239365f3db60b738868b1a5c9a946ef94dba
maithilijadhav <mjadhav@ualberta.ca>
2023-03-04 00:06:42 -0700

    Finished US 04.01.01 + US 04.02.01 -still might be bugs but overall done created User class to add User to database created Player class that identifies the user playing the app

--- app/src/main/java/com/example/codecatchers/MainActivity.java
+++ app/src/main/java/com/example/codecatchers/MainActivity.java
@@ -5,8 +5,12 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -17,10 +21,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView username;
+    Player currentUser = Player.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username_textview);
+        username.setText(currentUser.getUsername());
     }
 }
