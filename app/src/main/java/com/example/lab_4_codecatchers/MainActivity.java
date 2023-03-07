8dbb9440a28461707fa40ca3735f790eccc6e970
maithilijadhav <mjadhav@ualberta.ca>
2023-03-07 16:06:33 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
+++ app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView username;
-    User currentUser = User.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = findViewById(R.id.username_textview);
-        username.setText(currentUser.getUsername());
     }
 }
