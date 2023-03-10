4ed17018377b9f5d7d429eb77e00d22e236ca579
Neel <neel.patel0126@gmail.com>
2023-03-09 18:34:31 -0700

    Code with the camera being able to read a QR code. Edited only items related to the camera. moved Navras' hash code to CameraFragment. However user still needs to go into setting and approve the camera permission manually. the permission pop up doesn't show up. will look into it.

--- app/src/main/java/com/example/lab_4_codecatchers/CameraFragment.java
+++ app/src/main/java/com/example/lab_4_codecatchers/CameraFragment.java
@@ -2,13 +2,24 @@ package com.example.lab_4_codecatchers;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.example.lab_4_codecatchers.R;
+import com.google.zxing.Result;
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,14 @@ import com.example.lab_4_codecatchers.R;
  * create an instance of this fragment.
  */
 public class CameraFragment extends Fragment {
+    // declaring all the items from xml
+    TextView tv_textView;
+    TextView binary_textView;
+    TextView hash_textView;
+    TextView hashOut_textView;
+
+    // for camera - neel
+    private CodeScanner mCodeScanner;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +80,99 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        CodeScannerView scannerView = view.findViewById(R.id.scanner_view);
+        tv_textView = view.findViewById(R.id.tv_textView);
+        binary_textView = view.findViewById(R.id.binary_textView);
+        hash_textView = view.findViewById(R.id.hash_textView);
+        hashOut_textView = view.findViewById(R.id.hashOut_textView);
+
+        mCodeScanner = new CodeScanner(getActivity(), scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    // this function is to scna the QR code
+                    public void run() {
+                        /**
+                         TODO potentially create the other fragment here to that we can save the qr code
+                         */
+                        // gets the decoded QR content
+                        String s = result.getText();
+                        // shows it at tv_textView
+                        tv_textView.setText(result.getText());
+                        // gets the binary content of the decoded message
+                        byte[] bytes = s.getBytes();
+                        // sets the binary content at binary_textView
+                        binary_textView.setText(Arrays.toString(bytes));
+                        // gets the first item in the array and stores it as a string
+                        String x = String.valueOf(bytes[0]);
+                        // if the array is greater than size 1 than it goes throught it and added it to the string x
+                        if (bytes.length > 1 ) {
+                            for (int i = 1; i < bytes.length; i++) {
+                                x = x + String.valueOf(bytes[i]);
+                            }
+                        }
+                        // shows the string of x
+                        hash_textView.setText(x);
+                        // inputs x as the input for the hash function to get the hash output and sets it under hashOut_textView
+                        String hash_output = hash(x);
+                        hashOut_textView.setText(hash_output);
+                        /**
+                         * get rid of the extra items ^^^^^^^^
+                         */
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+        return view;
     }
+
+    // Returns the SHA-256 hash for any given string
+    // Based on code from http://www.java2s.com/example/android/java.lang/sha256-hash-string.html
+    public static String hash(String s) {
+        byte[] rawHash = null;
+        String output = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            rawHash = digest.digest(s.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("CodeCatchers", "Cannot calculate SHA-256");
+        }
+        if (rawHash != null) {
+            StringBuilder sb = new StringBuilder();
+            for (byte b: rawHash) {
+                sb.append(String.format("%02x", b));
+            }
+            output = sb.toString();
+        }
+        //Log.i("CodeCatchers", output);
+        return output;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_camera, container, false);
+//    }
 }
