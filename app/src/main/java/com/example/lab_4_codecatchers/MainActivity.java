6272cc43e62b5f907db4e9c3c2de84996a69075d
GhunaymY <116463354+GhunaymY@users.noreply.github.com>
2023-03-11 16:30:27 -0700

    Map Added

--- app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
+++ app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
@@ -1,34 +1,81 @@
 package com.example.lab_4_codecatchers;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.Manifest;
+import androidx.annotation.NonNull;
 
 import com.example.lab_4_codecatchers.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     User user;
+    private GoogleMap map;
 
     ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Check if the app has permission to access the user's location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // Permission has already been granted
+            // Do something with the location data
+        }
+
         //replaceFragment(new ProfileFragment()); // TODO: NEED to replace with camera or login fragment once impelmented
+
+        // start
+        // Check if camera permission has been granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        }
+//        else {
+//            // Permission has already been granted
+//            replaceFragment(new CameraFragment());
+//        }
+        // end
 
         //get data from firebase
         populatedUser();
 
         binding.navBar.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
-                case R.id.camera:
-                    replaceFragment(new CameraFragment());
+                case R.id.camera: // this case too
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        replaceFragment(new CameraFragment());
+                    } else {
+                        Toast.makeText(this, "Camera permission not granted", Toast.LENGTH_SHORT).show();
+                    }
                     break;
                 case R.id.map:
                     replaceFragment(new MapFragment());
@@ -43,8 +90,36 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-    }
 
+        // Get a reference to the map fragment
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        // Initialize the map
+        //mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted, do something with the location data
+            } else {
+                // Permission has been denied, handle this case
+            }
+        }
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //camera permission granted
+                //replaceFragment(new CameraFragment());
+            } else {
+                //camera permission denied
+                //handle this case, e.g. show an error message
+            }
+        }
+    }
     private void populatedUser() {
         // TODO: add the firebase stuff here (I just manually created a User for testing)
         ArrayList<Code> qrList = new ArrayList<Code>();
@@ -63,5 +138,11 @@ public class MainActivity extends AppCompatActivity {
 
     public User getUser() {
         return user;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        // TODO: Add map customizations here
     }
 }
