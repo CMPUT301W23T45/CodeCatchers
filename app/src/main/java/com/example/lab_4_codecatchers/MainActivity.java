87c17efde638d72f4484f51f811bc377754d6943
Brianna Stals <bstals@ualberta.ca>
2023-03-11 14:59:37 -0700

    Edits to fix merge conflict

--- app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
+++ app/src/main/java/com/example/lab_4_codecatchers/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.lab_4_codecatchers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.Manifest;
+import androidx.annotation.NonNull;
+
 
 import com.example.lab_4_codecatchers.databinding.ActivityMainBinding;
 
@@ -13,6 +20,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     User user;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
 
     ActivityMainBinding binding;
     @Override
@@ -20,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        // start
+        // Check if camera permission has been granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        }
+//        else {
+//            // Permission has already been granted
+//            replaceFragment(new CameraFragment());
+//            binding.navBar.getMenu().getItem(1).setChecked(true);
+//        }
+        // end
+        //get data from firebase
         replaceFragment(new CameraFragment());
         binding.navBar.getMenu().getItem(1).setChecked(true);
         //get data from firebase
@@ -28,9 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
         binding.navBar.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
-                case R.id.camera:
-                    replaceFragment(new CameraFragment());
-                    break;
+                case R.id.camera: // this case too
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        replaceFragment(new CameraFragment());
+                    } else {
+                        Toast.makeText(this, "Camera permission not granted", Toast.LENGTH_SHORT).show();
+                    }
                 case R.id.map:
                     replaceFragment(new MapFragment());
                     break;
@@ -44,6 +72,21 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //camera permission granted
+                //replaceFragment(new CameraFragment());
+            } else {
+                //camera permission denied
+                //handle this case, e.g. show an error message
+            }
+        }
     }
 
     private void populatedUser() {
