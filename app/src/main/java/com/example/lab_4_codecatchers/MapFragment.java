b09e6f3512e599b2a3cb43eefe10360668469a9d
NavrasK <trailmixerstudios@gmail.com>
2023-03-13 00:09:04 -0600

    I guess it's not crashing anymore, still don't have markers working yet...

--- app/src/main/java/com/example/lab_4_codecatchers/MapFragment.java
+++ app/src/main/java/com/example/lab_4_codecatchers/MapFragment.java
@@ -14,6 +14,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -30,7 +32,7 @@ import java.util.HashMap;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap map;
     private User user;
-    private HashMap<DocumentSnapshot, GeoPoint> allCodes = new HashMap<DocumentSnapshot, GeoPoint>();
+    private HashMap<String, GeoPoint> allCodes = new HashMap<String, GeoPoint>();
     // Default location at the UAlberta Computer Science building
     private LatLng defaultPosition = new LatLng(53.526790646055474, -113.52714133200335);
 
@@ -51,20 +53,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-//        map.moveCamera(CameraUpdateFactory.newLatLng(defaultPosition));
-//        FireStoreActivity db = FireStoreActivity.getInstance();
-//        db.getCodes().addOnSuccessListener(queryDocumentSnapshots -> {
-//            for (DocumentSnapshot code : queryDocumentSnapshots) {
-//                GeoPoint g = (GeoPoint) code.get("location");
-//                if (g.getLatitude() != 0.0 && g.getLongitude() != 0.0) {
-//                    allCodes.put(code, g);
-//                }
-//            }
-//        });
-//        for (DocumentSnapshot d: allCodes.keySet()) {
-//            LatLng position = new LatLng(allCodes.get(d).getLatitude(), allCodes.get(d).getLongitude());
-//            map.addMarker(new MarkerOptions().position(position).title(d.get("name").toString()));
-//        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPosition, 16F));
+        FireStoreActivity db = FireStoreActivity.getInstance();
+        db.getCodes().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (DocumentSnapshot code : queryDocumentSnapshots) {
+                GeoPoint g = (GeoPoint) code.get("location");
+                if (g.getLatitude() != 0.0 && g.getLongitude() != 0.0) {
+                    allCodes.put(code.get("name").toString(), g);
+                }
+            }
+        });
+        for (String n: allCodes.keySet()) {
+            LatLng position = new LatLng(allCodes.get(n).getLatitude(), allCodes.get(n).getLongitude());
+            map.addMarker(new MarkerOptions().position(position).title(n).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+            Log.i("CodeCatchers", n);
+        }
     }
 
     private void setUserInfo() {
