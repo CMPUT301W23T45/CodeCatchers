cf33d8b147a92b39965fa9ba4f5407928701901c
NavrasK <trailmixerstudios@gmail.com>
2023-03-12 23:54:37 -0600

    Working on map markers, running into crash when opening map

--- app/src/main/java/com/example/lab_4_codecatchers/MapFragment.java
+++ app/src/main/java/com/example/lab_4_codecatchers/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.lab_4_codecatchers;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +10,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.GeoPoint;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap map;
+    private User user;
+    private HashMap<DocumentSnapshot, GeoPoint> allCodes = new HashMap<DocumentSnapshot, GeoPoint>();
+    // Default location at the UAlberta Computer Science building
+    private LatLng defaultPosition = new LatLng(53.526790646055474, -113.52714133200335);
 
     @Nullable
     @Override
@@ -26,11 +41,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
+        setUserInfo();
         return rootView;
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
+//        map.moveCamera(CameraUpdateFactory.newLatLng(defaultPosition));
+//        FireStoreActivity db = FireStoreActivity.getInstance();
+//        db.getCodes().addOnSuccessListener(queryDocumentSnapshots -> {
+//            for (DocumentSnapshot code : queryDocumentSnapshots) {
+//                GeoPoint g = (GeoPoint) code.get("location");
+//                if (g.getLatitude() != 0.0 && g.getLongitude() != 0.0) {
+//                    allCodes.put(code, g);
+//                }
+//            }
+//        });
+//        for (DocumentSnapshot d: allCodes.keySet()) {
+//            LatLng position = new LatLng(allCodes.get(d).getLatitude(), allCodes.get(d).getLongitude());
+//            map.addMarker(new MarkerOptions().position(position).title(d.get("name").toString()));
+//        }
+    }
+
+    private void setUserInfo() {
+        user = User.getInstance();
     }
 }
