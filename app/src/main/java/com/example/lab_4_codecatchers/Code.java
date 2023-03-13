1af4d3bc97bd57f27083bdfbfa5942084566a134
NavrasK <trailmixerstudios@gmail.com>
2023-03-13 01:04:27 -0600

    Added human readable name generator based on hash

--- app/src/main/java/com/example/lab_4_codecatchers/Code.java
+++ app/src/main/java/com/example/lab_4_codecatchers/Code.java
@@ -1,5 +1,9 @@
 package com.example.lab_4_codecatchers;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+
 /**
  * Representation of a QR Code or Barcode
  */
@@ -8,6 +12,7 @@ public class Code {
     private String hash;
     private String humanName;
     private int humanImage;
+    private final ArrayList<String[]> opposites = new ArrayList<>();
 
     //CONSTRUCTORS
 
@@ -32,7 +37,7 @@ public class Code {
     public Code(int score, String hash) {
         this.score = score;
         this.hash = hash;
-        this.humanName = "Jim"; // TODO: change to nameFunction once implemented
+        this.humanName = generateHumanName(hash); // TODO: change to nameFunction once implemented
         this.humanImage = R.drawable.baseline_qr_code_2_24; // TODO: change to imageFunction once implemented
     }
 
@@ -84,4 +89,31 @@ public class Code {
     }
 
     //FUNCTIONS
+    private String generateHumanName(String hash) {
+        String[] mentalAdjectives = {"Clever", "Intelligent", "Inquisitive", "Curious", "Creative", "Wise", "Analytical", "Perceptive", "Insightful", "Reflective", "Contemplative", "Philosophical", "Eloquent", "Expressive", "Imaginative", "Visionary"};
+        String[] physicalAdjectives = {"Slender", "Muscular", "Chubby", "Obese", "Lean", "Fit", "Tall", "Short", "Curvy", "Athletic", "Stout", "Brawny", "Fragile", "Graceful", "Flabby", "Lanky"};
+        String[] colours = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "Brown", "Gray", "Black", "White", "Turquoise", "Magenta", "Gold", "Silver", "Bronze"};
+        String[] gemstones = {"Diamond", "Ruby", "Emerald", "Sapphire", "Topaz", "Opal", "Amethyst", "Garnet", "Peridot", "Aquamarine", "Pearl", "Turquoise", "Citrine", "Agate", "Quartz", "Jade"};
+        String[] animals = {"Lion", "Tiger", "Elephant", "Giraffe", "Gorilla", "Kangaroo", "Panda", "Squirrel", "Koala", "Lemur", "Platypus", "Hedgehog", "Sloth", "Meerkat", "Penguin", "Ostrich"};
+
+        String output = "";
+        String substr = hash.substring(0, 5);
+        for (int i = 0; i < substr.length(); i++) {
+            int index = Character.digit(substr.charAt(i), 16);
+            String selection;
+            if (i == 0) {
+                selection = mentalAdjectives[index];
+            } else if (i == 1) {
+                selection = physicalAdjectives[index];
+            } else if (i == 2) {
+                selection = colours[index];
+            } else if (i == 3) {
+                selection = gemstones[index];
+            } else {
+                selection = animals[index];
+            }
+            output = output.concat(selection);
+        }
+        return output;
+    }
 }
